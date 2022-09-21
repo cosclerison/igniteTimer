e837bd01e8cd3679da7ce94eb29554d02cdcee36
Clerison <cosclerison@gmail.com>
2022-09-21 18:52:53 -0300

    testando theme e styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/button";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
   return (
-      <h1>React JS!</h1>    
+      <ThemeProvider theme={defaultTheme}>
+        <Button variant="primary" />
+        <Button variant="secondary" />
+        <Button variant="danger" />
+        <Button variant="success" />
+        <Button />
+      </ThemeProvider>
   )
 }
