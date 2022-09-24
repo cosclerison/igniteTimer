f67f80e36b3dbde9d218083816ad4a3329c30840
Clerison <cosclerison@gmail.com>
2022-09-23 22:46:49 -0300

    Criando Layout de paginas

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
+import { DefaultLayouts } from './layouts/Default.layouts'
 import { History } from './pages/History'
 import { Home } from './pages/Home'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayouts />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
